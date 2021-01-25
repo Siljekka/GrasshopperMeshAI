10cac18c99b27009f8f0d1fbf229f5a0d7c8f06d
Silje Knutsvik Kalleberg <siljekka@ntnu.no>
2021-01-25 20:17:31 +0100

    Add Hilde's work #2

--- MeshPoints/MeshQuality.cs
+++ MeshPoints/MeshQuality.cs
@@ -10,8 +10,22 @@ namespace MeshPoints
 {
     class MeshQuality
     {
-        public double sideRatio;
-        public double angleRatio;
-        public MeshFace meshFace;
+        //Properties
+        public List<double> aspectRatio { get; set; }
+        public List<double> angleRatio { get; set; }
+        public MeshFace meshFace { get; set; }
+
+        //Constructor
+        public MeshQuality()
+        {
+            //Empty constructor
+        }
+        public MeshQuality(List<double> _aspectRatio, List<double> _angleRatio)
+        {
+            aspectRatio = _aspectRatio;
+            angleRatio = _angleRatio;
+        }
+
+        //Methods
     }
 }
