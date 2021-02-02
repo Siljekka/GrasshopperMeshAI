dd817480593896bc60c224b5b12afacbf5943d65
Silje Knutsvik Kalleberg <siljekka@ntnu.no>
2021-02-02 10:41:18 +0100

    Created the classes Element, Mesh2d, Mesh3d

---
+++ MeshPoints/Mesh3D.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MeshPoints
+{
+    class Mesh3D
+    {
+        public List<Element> Elements { get; set; } //list of elements
+        public List<Node> Nodes { get; set; } //list of nodes
+        public int Nx { get; set; } //number of nodes in x-dir
+        public int Ny { get; set; } //number of nodes in y-dir
+        public int Nz { get; set; } //number of nodes in z-dir
+
+        public Mesh3D()
+        {
+            //Empty constructor
+        }
+
+    }
+}
