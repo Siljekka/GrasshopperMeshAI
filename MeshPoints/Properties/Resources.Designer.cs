2a25737c3926e8d081a2a23b1264e4c304518743
hnedl <hnedl@LAPTOP-TMJQPLI9>
2021-02-03 23:29:00 +0100

    Not finished yet - Mesh3D

--- MeshPoints/Properties/Resources.Designer.cs
+++ MeshPoints/Properties/Resources.Designer.cs
@@ -63,6 +63,16 @@ namespace MeshPoints.Properties {
         /// <summary>
         ///   Looks up a localized resource of type System.Drawing.Bitmap.
         /// </summary>
+        internal static System.Drawing.Bitmap mountain {
+            get {
+                object obj = ResourceManager.GetObject("mountain", resourceCulture);
+                return ((System.Drawing.Bitmap)(obj));
+            }
+        }
+        
+        /// <summary>
+        ///   Looks up a localized resource of type System.Drawing.Bitmap.
+        /// </summary>
         internal static System.Drawing.Bitmap test {
             get {
                 object obj = ResourceManager.GetObject("test", resourceCulture);
