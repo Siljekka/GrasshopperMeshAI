87047f860de6e512c5e3777ce8b68b9f69ecc911
Magnus Kunnas <kunmagg@users.noreply.github.com>
2021-02-18 19:39:39 +0100

    Add icon to MeshQuality-component

--- MeshPoints/MeshQuality/MeshQuality.cs
+++ MeshPoints/MeshQuality/MeshQuality.cs
@@ -242,9 +242,7 @@ namespace MeshPoints
         {
             get
             {
-                //You can add image files to your project resources and access them like this:
-                // return Resources.IconForThisComponent;
-                return null;
+                return Properties.Resources.MeshQuality;
             }
         }
 
--- MeshPoints/Properties/Resources.Designer.cs
+++ MeshPoints/Properties/Resources.Designer.cs
@@ -63,6 +63,16 @@ namespace MeshPoints.Properties {
         /// <summary>
         ///   Looks up a localized resource of type System.Drawing.Bitmap.
         /// </summary>
+        internal static System.Drawing.Bitmap MeshQuality {
+            get {
+                object obj = ResourceManager.GetObject("MeshQuality", resourceCulture);
+                return ((System.Drawing.Bitmap)(obj));
+            }
+        }
+        
+        /// <summary>
+        ///   Looks up a localized resource of type System.Drawing.Bitmap.
+        /// </summary>
         internal static System.Drawing.Bitmap mountain {
             get {
                 object obj = ResourceManager.GetObject("mountain", resourceCulture);
