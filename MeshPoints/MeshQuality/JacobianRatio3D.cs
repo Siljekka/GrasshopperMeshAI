ea55564fe76f89b44849674e0600c58990d03d9d
Magnus Kunnas <kunmagg@users.noreply.github.com>
2021-03-09 14:33:16 +0100

    Optimize Jacobian3d component
    Removed all symbolic operations by explicitly defining the shape functions' partial derivatives.

--- MeshPoints/MeshQuality/JacobianRatio3D.cs
+++ MeshPoints/MeshQuality/JacobianRatio3D.cs
@@ -48,21 +48,15 @@ namespace MeshPoints.MeshQuality
             Mesh3D mesh3D = new Mesh3D();
             DA.GetData(0, ref mesh3D);
 
-            List<double> jacobianRatios = new List<double>();
-            foreach(Element e in mesh3D.Elements)
-            {
-                jacobianRatios.Add(CalculateJacobianRatioOf8NodeElement(e));
-            }
+            List<double> jacobianRatios = CalculateJacobianRatioOfmesh3D(mesh3D);
 
             // Output
-            DA.SetData(0, jacobianRatios);
+            DA.SetDataList(0, jacobianRatios);
 
         }
 
-        private double CalculateJacobianRatioOf8NodeElement(Element e)
+        private List<double> CalculateJacobianRatioOfmesh3D(Mesh3D mesh)
         {
-            AddRuntimeMessage(GH_RuntimeMessageLevel.Warning, "Function 'CalculateJacobianRatioOf8NodeElement' not implemented.");
-
             // Corner nodes of the isoparametric element
             var naturalNodes = new List<List<Double>>
             {
@@ -70,101 +64,123 @@ namespace MeshPoints.MeshQuality
                 new List<double> { -1, -1, 1 }, new List<double> { 1, -1, 1 }, new List<double> { 1, 1, 1 }, new List<double> { -1, 1, 1 }
             };
 
-            // Global X, Y, and Z-coordinates of the corner nodes of the actual element
-            List<double> gX = new List<double>()
+            var jacobianRatios = new List<double>();
+            foreach (Element e in mesh.Elements)
             {
-                e.Node1.Coordinate.X, e.Node2.Coordinate.X, e.Node3.Coordinate.X, e.Node4.Coordinate.X,
-                e.Node5.Coordinate.X, e.Node6.Coordinate.X, e.Node7.Coordinate.X, e.Node8.Coordinate.X
-            };
-            List<double> gY = new List<double>()
-            {
-                e.Node1.Coordinate.Y, e.Node2.Coordinate.Y, e.Node3.Coordinate.Y, e.Node4.Coordinate.Y,
-                e.Node5.Coordinate.Y, e.Node6.Coordinate.Y, e.Node7.Coordinate.Y, e.Node8.Coordinate.Y
-            };
-            List<double> gZ = new List<double>()
-            {
-                e.Node1.Coordinate.Z, e.Node2.Coordinate.Z, e.Node3.Coordinate.Z, e.Node4.Coordinate.Z,
-                e.Node5.Coordinate.Z, e.Node6.Coordinate.Z, e.Node7.Coordinate.Z, e.Node8.Coordinate.Z
-            };
-
-            // Declare the three variables of the natural coordinate system
-            var r = Expr.Variable("r"); // xi
-            var s = Expr.Variable("s"); // nu
-            var t = Expr.Variable("t"); // zeta
-
-            // Shape functions
-            Expr N1 = 0.125 * (1 - r) * (1 - s) * (1 - t);
-            Expr N2 = 0.125 * (1 + r) * (1 - s) * (1 - t);
-            Expr N3 = 0.125 * (1 + r) * (1 + s) * (1 - t);
-            Expr N4 = 0.125 * (1 - r) * (1 + s) * (1 - t);
-            Expr N5 = 0.125 * (1 - r) * (1 - s) * (1 + t);
-            Expr N6 = 0.125 * (1 + r) * (1 - s) * (1 + t);
-            Expr N7 = 0.125 * (1 + r) * (1 + s) * (1 + t);
-            Expr N8 = 0.125 * (1 - r) * (1 + s) * (1 + t);
-
-            // The function f(x, y, z) deconstructed into its x, y, and z-terms
-            Expr x = N1 * gX[0] + N2 * gX[1] + N3 * gX[2] + N4 * gX[3] + N5 * gX[4] + N6 * gX[5] + N7 * gX[6] + N8 * gX[7];
-            Expr y = N1 * gY[0] + N2 * gY[1] + N3 * gY[2] + N4 * gY[3] + N5 * gY[4] + N6 * gY[5] + N7 * gY[6] + N8 * gY[7];
-            Expr z = N1 * gZ[0] + N2 * gZ[1] + N3 * gZ[2] + N4 * gZ[3] + N5 * gZ[4] + N6 * gZ[5] + N7 * gZ[6] + N8 * gZ[7];
-
-            // The Jacobian matrix of f is the matrix of all its first-order partial derivatives
-            Expr xDr = x.Differentiate(r);
-            Expr xDs = x.Differentiate(s);
-            Expr xDt = x.Differentiate(t);
-                      
-            Expr yDr = y.Differentiate(r);
-            Expr yDs = y.Differentiate(s);
-            Expr yDt = y.Differentiate(t);
-                      
-            Expr zDr = z.Differentiate(r);
-            Expr zDs = z.Differentiate(s);
-            Expr zDt = z.Differentiate(t);
-
-            List<Expr> derivatives = new List<Expr>
-            {
-                xDr, xDs, xDt, yDr, yDs, yDt, zDs, zDr, zDt
-            };
-
-            List<double> jacobiansOfElement = new List<double>();
-            foreach (List<Double> node in naturalNodes)
-            {
-                // Substitute the natural coordinates of a given node into the symbolic expression
-                var naturalCoords = new Dictionary<string, FloatingPoint>
+                // Global X, Y, and Z-coordinates of the corner nodes of the actual element
+                List<double> gX = new List<double>()
                 {
-                    { "r", node[0] },
-                    { "s", node[1] },
-                    { "t", node[2] },
+                    e.Node1.Coordinate.X, e.Node2.Coordinate.X, e.Node3.Coordinate.X, e.Node4.Coordinate.X,
+                    e.Node5.Coordinate.X, e.Node6.Coordinate.X, e.Node7.Coordinate.X, e.Node8.Coordinate.X
+                };
+                List<double> gY = new List<double>()
+                {
+                    e.Node1.Coordinate.Y, e.Node2.Coordinate.Y, e.Node3.Coordinate.Y, e.Node4.Coordinate.Y,
+                    e.Node5.Coordinate.Y, e.Node6.Coordinate.Y, e.Node7.Coordinate.Y, e.Node8.Coordinate.Y
+                };
+                List<double> gZ = new List<double>()
+                {
+                    e.Node1.Coordinate.Z, e.Node2.Coordinate.Z, e.Node3.Coordinate.Z, e.Node4.Coordinate.Z,
+                    e.Node5.Coordinate.Z, e.Node6.Coordinate.Z, e.Node7.Coordinate.Z, e.Node8.Coordinate.Z
                 };
 
-                    
-                // Evaluates each partial derivative in the coordinates of the node, and outputs as a double (RealValue).
-                var calcDerivs = new List<Double>();
-                foreach (var deriv in derivatives)
+                List<double> jacobiansOfElement = new List<double>();
+                foreach (List<Double> node in naturalNodes)
                 {
-                    calcDerivs.Add(deriv.Evaluate(naturalCoords).RealValue);
+                    // Substitute the natural coordinates into the symbolic expression
+                    var r = node[0];
+                    var s = node[1];
+                    var t = node[2];
+
+                    // Partial derivatives of the shape functions
+                    var N1Dr = -0.125 * (s - 1) * (t - 1);
+                    var N1Ds = -0.125 * (r - 1) * (t - 1);
+                    var N1Dt = -0.125 * (r - 1) * (s - 1);
+                    var N2Dr = 0.125 * (s - 1) * (t - 1);
+                    var N2Ds = 0.125 * (r + 1) * (t - 1);
+                    var N2Dt = 0.125 * (r + 1) * (s - 1);
+                    var N3Dr = -0.125 * (s + 1) * (t - 1);
+                    var N3Ds = -0.125 * (r + 1) * (t - 1);
+                    var N3Dt = -0.125 * (r + 1) * (s + 1);
+                    var N4Dr = 0.125 * (s + 1) * (t - 1);
+                    var N4Ds = 0.125 * (r - 1) * (t - 1);
+                    var N4Dt = 0.125 * (r - 1) * (s + 1);
+                    var N5Dr = 0.125 * (s - 1) * (t + 1);
+                    var N5Ds = 0.125 * (r - 1) * (t + 1);
+                    var N5Dt = 0.125 * (r - 1) * (s - 1);
+                    var N6Dr = -0.125 * (s - 1) * (t + 1);
+                    var N6Ds = -0.125 * (r + 1) * (t + 1);
+                    var N6Dt = -0.125 * (r + 1) * (s - 1);
+                    var N7Dr = 0.125 * (s + 1) * (t + 1);
+                    var N7Ds = 0.125 * (r + 1) * (t + 1);
+                    var N7Dt = 0.125 * (r + 1) * (s + 1);
+                    var N8Dr = -0.125 * (s + 1) * (t + 1);
+                    var N8Ds = -0.125 * (r - 1) * (t + 1);
+                    var N8Dt = -0.125 * (r - 1) * (s + 1);
+
+                    var sfDr = new List<double>
+                    {
+                        N1Dr, N2Dr, N3Dr, N4Dr, N5Dr, N6Dr, N7Dr, N8Dr
+                    };
+                    var sfDs = new List<double>
+                    {
+                        N1Ds, N2Ds, N3Ds, N4Ds, N5Ds, N6Ds, N7Ds, N8Ds
+                    };
+                    var sfDt = new List<double>
+                    {
+                        N1Dt, N2Dt, N3Dt, N4Dt, N5Dt, N6Dt, N7Dt, N8Dt
+                    };
+
+                    // Evaluates each partial derivative in the isoparametric node
+                    var calcDerivs = new List<Double>
+                    {
+                        MultiplyLists(gX, sfDr),
+                        MultiplyLists(gX, sfDs),
+                        MultiplyLists(gX, sfDt),
+
+                        MultiplyLists(gY, sfDr),
+                        MultiplyLists(gY, sfDs),
+                        MultiplyLists(gY, sfDt),
+
+                        MultiplyLists(gZ, sfDr),
+                        MultiplyLists(gZ, sfDs),
+                        MultiplyLists(gZ, sfDt)
+                    };
+
+                    // Helper function to piecewise multiply elements of two lists of length 8
+                    double MultiplyLists(List<double> a, List<double> b)
+                    {
+                        double sum = 0.0;
+                        for (int i = 0; i<8; i++)
+                        {
+                            sum += (a[i] * b[i]);
+                        }
+                        return sum;
+                    }
+
+                    // Structure data in the form of a Jacobian matrix
+                    Matrix<double> jacobianMatrix = DenseMatrixModule.ofArray2(new double[,]
+                    {
+                        {calcDerivs[0], calcDerivs[3], calcDerivs[6] },
+                        {calcDerivs[1], calcDerivs[4], calcDerivs[7] },
+                        {calcDerivs[2], calcDerivs[5], calcDerivs[8] },
+                    });
+
+                    var jacobianDeterminant = jacobianMatrix.Determinant();
+                    jacobiansOfElement.Add(Math.Abs(jacobianDeterminant));
+
+                    // This might indicate something wrong with the ordering of nodes in elements (???)
+                    if (jacobianDeterminant < 0) {
+                        AddRuntimeMessage(GH_RuntimeMessageLevel.Remark, "The Jacobian is negative.");
+                    }
                 }
 
-                // Structure data in the form of a Jacobian matrix
-                Matrix<double> jacobianMatrix = DenseMatrixModule.ofArray2(new double[,]
-                {
-                    {calcDerivs[0], calcDerivs[1], calcDerivs[2] },
-                    {calcDerivs[3], calcDerivs[4], calcDerivs[5] },
-                    {calcDerivs[6], calcDerivs[7], calcDerivs[8] },
-                });
+                // A value of 1 denotes a cuboid element.
+                double jacobianRatio = jacobiansOfElement.Min() / jacobiansOfElement.Max();
 
-                jacobiansOfElement.Add(jacobianMatrix.Determinant());
+                jacobianRatios.Add(jacobianRatio);
             }
-
-            // Minimum element divided by maximum element. A value of 1 denotes a rectangular element.
-            double jacobianRatio = jacobiansOfElement.Min() / jacobiansOfElement.Max();
-
-            // Generate a warning if Jacobian ratio is outside of the valid range: [0.0, 1.0].
-            if (jacobianRatio < 0 || 1.0 < jacobianRatio)
-            {
-                AddRuntimeMessage(GH_RuntimeMessageLevel.Warning, "The Jacobian ratio is outside of the valid range [0.0, 1.0]. A negative value might indicate a concave or self-intersecting element.");
-            }
-
-            return jacobianRatio;
+            return jacobianRatios;
         }
 
         /// <summary>
