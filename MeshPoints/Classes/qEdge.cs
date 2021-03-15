24bdb68785315d04cb1a637a9ee2a36644233f3f
Silje Knutsvik Kalleberg <siljekka@ntnu.no>
2021-03-15 12:08:04 +0100

    Update qEdge

--- MeshPoints/Classes/qEdge.cs
+++ MeshPoints/Classes/qEdge.cs
@@ -14,8 +14,8 @@ namespace MeshPoints.Classes
         public int Index { get; set; }
         public qNode StartNode { get; set; }
         public qNode EndNode { get; set; }
-        public double Length { get; }
-        public Line EdgeLine { get; } // makes the user see the edge, might delete?
+        public double Length { get; set; }
+        public Line EdgeLine { get; set; } // makes the user see the edge, might delete?
         public qElement Element1 { get; set; }
         public qElement Element2 { get; set; }
         public qEdge LeftFrontNeighbor { get; set; }
@@ -37,11 +37,11 @@ namespace MeshPoints.Classes
             EdgeLine = VisualizeLine(_startNode, _endNode);
         }
 
-        private double CalculateLength(qNode _startNode, qNode _endNode)
+        public double CalculateLength(qNode _startNode, qNode _endNode)
         {
             return _startNode.Coordinate.DistanceTo(_endNode.Coordinate);
         }
-        private Line VisualizeLine(qNode _startNode, qNode _endNode)
+        public Line VisualizeLine(qNode _startNode, qNode _endNode)
         {
             return new Line(_startNode.Coordinate, _endNode.Coordinate);
         }
