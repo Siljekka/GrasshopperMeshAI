43173887b3214f6b59b9b122c6b8922381fdde41
Magnus Kunnas <kunmagg@users.noreply.github.com>
2021-03-28 16:00:44 +0200

    Use scipy for procrustes
    This works, but it does not give us the scaling factor needed for edge lengths. Using this commit to save my work before re-writing own implementation based on scipy-version.

--- MeshPoints/MachineLearning/contour_transform.py
+++ MeshPoints/MachineLearning/contour_transform.py
@@ -1,6 +1,6 @@
 from math import cos, sin, pi
 import numpy as np
-from scipy import linalg
+from scipy import linalg, spatial
 import matplotlib.pyplot as plt
 
 
@@ -20,7 +20,6 @@ def create_regular_ngon(number_of_sides: int):
 
 def create_random_ngon(number_of_sides: int):
 
-
     pass
 
 
@@ -30,29 +29,32 @@ def procrustes_superimposition(contour: np.array):
 
     # Calculate the mean coordinate values of input contour, P, and regular polygon, Q
     p_mean = np.array([np.sum(contour[0]), np.sum(contour[1])])/n
-    q_mean = [np.sum(reg_ngon[0])/n, np.sum(reg_ngon[1])/n]  # i think this is ~0 always, so we could remove it
+    # i think this is ~0 always, so we could remove it
+    q_mean = [np.sum(reg_ngon[0])/n, np.sum(reg_ngon[1])/n]
 
     # Calculate the "centered Euclidean norms" of P and Q
     # todo: is this a scalar or a vector (of scalars) ???
     p_norm = []
     q_norm = []
     for i in range(n):
-        p_norm.append((contour[0][i] - p_mean[0])**2 + (contour[1][i] - p_mean[1])**2)
+        p_norm.append((contour[0][i] - p_mean[0])**2 +
+                      (contour[1][i] - p_mean[1])**2)
         q_norm.append((reg_ngon[0][i])**2 + (reg_ngon[1][i])**2)
 
     p_norm = sum(p_norm)
     q_norm = sum(q_norm)
 
     # Scale P* and Q by the norms
-    p_scaled=np.zeros((2,n))
-    q_scaled=np.zeros((2,n))
-    for i in range(n):
-        p_scaled[0][i] = contour[0][i]/p_norm
-        p_scaled[1][i] = contour[1][i]/p_norm
-        q_scaled[0][i] = reg_ngon[0][i]/q_norm
-        q_scaled[1][i] = reg_ngon[1][i]/q_norm
+    # p_scaled=np.zeros((2,n))
+    # q_scaled=np.zeros((2,n))
+    # for i in range(n):
+    #     p_scaled[0][i] = contour[0][i]/p_norm
+    #     p_scaled[1][i] = contour[1][i]/p_norm
+    #     q_scaled[0][i] = reg_ngon[0][i]/q_norm
+    #     q_scaled[1][i] = reg_ngon[1][i]/q_norm
 
-
+    p_scaled = contour/p_norm
+    q_scaled = reg_ngon/q_norm
     # q_scaled = np.array([reg_ngon[i]/p_norm[i] for i in range(n)])
     # q_scaled = np.array([reg_ngon[0]/q_norm, reg_ngon[1]/q_norm])
 
@@ -65,17 +67,36 @@ def procrustes_superimposition(contour: np.array):
     scaling_factor = q_norm / np.sum(C)
 
     # R = U*V^T
-    rotation_matrix =  U @ Vt
+    rotation_matrix = U @ Vt
 
     # The coordinates of the transformed contour are given by P_trans = scaling_factor * p_scaled * rotation + q_mean
     # We add the q_mean values separately
-    tmp =  (scaling_factor * p_scaled) @ rotation_matrix
+    tmp = (scaling_factor * p_scaled) @ rotation_matrix
     tc_x = tmp[0] + q_mean[0]
     tc_y = tmp[1] + q_mean[1]
     transformed_contour = np.array([tc_x, tc_y])
     plot_ngon(q_scaled)
     plot_ngon(p_scaled)
     return {"transformed_contour": transformed_contour, "scaling_factor": scaling_factor}
+
+
+def procrustes_superimposition_scipy(contour: np.array):
+    n = contour.shape[1]
+    reg_ngon = create_regular_ngon(n)
+
+    # have to transform list of points to [x1, y1] ... [xn, yn]-shape
+    # not [x1... xn], [y1 ... yn]
+
+    reshaped_c =[]
+    reshaped_ngon = []
+    for i in range(n):
+        reshaped_c.append([contour[0][i], contour[1][i]])
+        reshaped_ngon.append([reg_ngon[0][i], reg_ngon[1][i]])
+
+
+    transformed_contour = spatial.procrustes(reshaped_ngon, reshaped_c)
+
+    return transformed_contour
 
 
 def plot_ngon(np_coords: np.array):
--- MeshPoints/MachineLearning/main.py
+++ MeshPoints/MachineLearning/main.py
@@ -3,11 +3,15 @@ import numpy as np
 import matplotlib.pyplot as plt
 
 if __name__ == '__main__':
-    test_contour = np.array([[2.02, 0, -2.14, -2.34, -0.24, 1.78], [1.52, 3, 1.64, -0.78, -2.18, -0.5]])
+    test_contour = np.array(
+        [[7.06, 9.16, 9.62, 14.2, 14.36, 10.46], [0.55, -0.99, -3.25, -0.95, 3.31, 3.53]])
 
-    transformed_contour = ct.procrustes_superimposition(test_contour)
+
+    transformed_contour = ct.procrustes_superimposition_scipy(test_contour)
     # ct.plot_ngon(test_contour)
-    ct.plot_ngon(transformed_contour['transformed_contour'])
+    ct.plot_ngon(np.array([[x[0] for x in transformed_contour[1]], [y[1] for y in transformed_contour[1]]]))
+    ct.plot_ngon(np.array([[x[0] for x in transformed_contour[0]], [y[1] for y in transformed_contour[0]]]))
+    # ct.plot_ngon(transformed_contour['transformed_contour'])
     plt.show()
 
     print(transformed_contour)
