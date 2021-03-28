7d5d1f9fec9022664e5f28d398317a0868a6c479
Magnus Kunnas <kunmagg@users.noreply.github.com>
2021-03-28 21:02:45 +0200

    Change scale output of procrustes to return total scaling of contour
    Previous implementation returned the scaling factor of the SVD, which is not the same as the total scaling of the contour.
    The now returned scale should be the correct one as asserted on line 54.

--- MeshPoints/MachineLearning/pre_processing.py
+++ MeshPoints/MachineLearning/pre_processing.py
@@ -1,7 +1,7 @@
 from math import cos, sin, pi
 from random import random
 import numpy as np
-from scipy import linalg, spatial
+from scipy import linalg
 import matplotlib.pyplot as plt
 
 
@@ -12,6 +12,7 @@ def procrustes(contour: np.array) -> dict:
     recognition in the neural networks.
 
     The input contour, P*, with n sides is transformed to match a reference regular ngon, Q.
+    The scaling factor says how much to scale the input contour to get the transformed contour
 
     [1] Papagiannopoulos, A.; Clausen, P., Avellan, F. (2020).
         "How to teach neural networks to mesh: Application to 2-D simplical contours"
@@ -22,31 +23,38 @@ def procrustes(contour: np.array) -> dict:
 
     # Translate P* to the origin.
     p_mean = contour - np.mean(contour, 0)
-    assert(sum(np.mean(reference, 0)) < 0.000001)  # As Q is defined in the origin, we do not have to move it
-    q_mean = reference - np.mean(reference, 0)
+    # As Q is defined in the origin, we do not have to calculate q_mean
+    # assert (sum(np.mean(reference, 0)) < 0.000001)
+    # q_mean = reference - np.mean(reference, 0)
+    q_mean = reference
 
     # Calculate the "centered Euclidean norms" of P* and Q
     p_norm = np.linalg.norm(p_mean)
     q_norm = np.linalg.norm(q_mean)
 
     # Scale P* and Q by the norms
-    p_scaled = p_mean/p_norm
-    q_scaled = q_mean/q_norm
+    p_scaled = p_mean / p_norm
+    q_scaled = q_mean / q_norm
 
     # Apply "Singular Value Decomposition (SVD)" to A = q_scaled.T * p_scaled => A = UCV^T
-    A = q_scaled.T @ p_scaled
-    U, C, Vt = linalg.svd(A)
+    a = q_scaled.T @ p_scaled
+    u, c, vt = linalg.svd(a)
 
-    # Get scaling factor, S = q_norm * trace(C); trace(C) = sum(C) in scipy.linalg.svd.
-    scaling_factor = q_norm * np.sum(C)
+    # Get the sigma, S = q_norm * trace(C); trace(C) = sum(C) in scipy.linalg.svd. Aka. scaling factor.
+    sigma = q_norm * np.sum(c)
 
     # Get optimal rotation matrix, R = U*V^T
-    rotation_matrix = U @ Vt
+    rotation_matrix = u @ vt
 
     # The coordinates of the transformed contour are given by P_trans = scaling_factor * p_scaled * rotation + ~q
-    transformed_contour = scaling_factor * p_scaled @ rotation_matrix
+    # p_norm*p_scaled
+    transformed_contour = sigma * p_scaled @ rotation_matrix
 
-    return {"transformed_contour": transformed_contour, "scaling_factor": scaling_factor}
+    # The scale difference between the transformed and the original contour is s / p_norm;
+    # assert (np.allclose(contour, p_norm * p_scaled + np.mean(contour, 0)))
+    scale = sigma / p_norm
+
+    return {"transformed_contour": transformed_contour, "scale": scale}
 
 
 def create_regular_ngon(number_of_sides: int) -> np.array:
@@ -83,11 +91,14 @@ def create_random_ngon(number_of_sides: int) -> np.array:
 
     return np.array(polygon)
 
-def plot_polygon(np_coords: np.array) -> None:
+
+def plot_polygon(np_coords: np.array, style='') -> None:
+    """
+    See https://matplotlib.org/2.1.1/api/_as_gen/matplotlib.pyplot.plot.html for styles.
+    """
     # Reshaping input and appending the first element to get a circular plot of the polygons
     coords = [[x[0] for x in np_coords], [y[1] for y in np_coords]]
     coords[0].append(coords[0][0])
     coords[1].append(coords[1][0])
 
-    plt.plot(coords[0], coords[1])
-
+    plt.plot(coords[0], coords[1], style)
