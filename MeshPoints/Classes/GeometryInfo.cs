31e832c7ba7d7121b9fb86cfa59c876ba29197fd
Silje Knutsvik Kalleberg <siljekka@ntnu.no>
2021-04-14 13:21:59 +0200

    Add GeometryInfo class

---
+++ MeshPoints/Classes/GeometryInfo.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Rhino.Geometry;
+
+namespace MeshPoints.Classes
+{
+    class GeometryInfo
+    {
+        public List<BrepFace> Faces { get; set; }
+        public List<BrepEdge> Edges { get; set; }
+        public List<BrepVertex> Vertices { get; set; }
+
+
+        public GeometryInfo()
+        { 
+            // empty constructor
+        }
+    }
+}
