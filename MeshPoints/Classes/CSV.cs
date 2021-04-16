f2c401be266aebc5358bc0af358fee8603abc99b
hnedl <hnedl@LAPTOP-TMJQPLI9>
2021-04-16 13:20:56 +0200

    Make new class

---
+++ MeshPoints/Classes/CSV.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MeshPoints.Classes
+{
+    class CSV
+    {
+        /*Referer til: https://www.youtube.com/watch?v=vDpww7HsdnM */
+        public static void addRecord(string v1, string v2, string v3, string v4)
+        {
+            try
+            { 
+            }
+
+        }
+    }
+}
