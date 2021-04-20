d015783a07d7ff151366551c1f666f366e04c299
hnedl <hnedl@LAPTOP-TMJQPLI9>
2021-04-20 15:35:07 +0200

    Change name of component

---
+++ MeshPoints/DeconstructClasses/DeconstructSmartMesh.cs
@@ -0,0 +1,79 @@
+﻿using Grasshopper.Kernel;
+using Rhino.Geometry;
+using System;
+using System.Collections.Generic;
+using MeshPoints.Classes;
+
+namespace MeshPoints.DeconstructClasses
+{
+    public class DeconstructSmartMesh : GH_Component
+    {
+        /// <summary>
+        /// Initializes a new instance of the DeconstructMesh3d class.
+        /// </summary>
+        public DeconstructSmartMesh()
+          : base("Deconstruct SmartMesh", "decMesh",
+              "Deconstructing SmartMesh class",
+              "MyPlugIn", "Deconstruct")
+        {
+        }
+
+        /// <summary>
+        /// Registers all the input parameters for this component.
+        /// </summary>
+        protected override void RegisterInputParams(GH_Component.GH_InputParamManager pManager)
+        {
+            pManager.AddGenericParameter("SmartMesh", "mesh", "SmartMesh class", GH_ParamAccess.item);
+        }
+
+        /// <summary>
+        /// Registers all the output parameters for this component.
+        /// </summary>
+        protected override void RegisterOutputParams(GH_Component.GH_OutputParamManager pManager)
+        {
+            pManager.AddGenericParameter("Elements", "e", "List of elements", GH_ParamAccess.list); 
+            pManager.AddGenericParameter("Nodes", "n", "List of nodes", GH_ParamAccess.list); 
+            pManager.AddGenericParameter("Geometry", "geo", "Geometry information", GH_ParamAccess.item); 
+            pManager.AddGenericParameter("Mesh", "m", "Mesh", GH_ParamAccess.item); 
+
+        }
+
+        /// <summary>
+        /// This is the method that actually does the work.
+        /// </summary>
+        /// <param name="DA">The DA object is used to retrieve from inputs and store in outputs.</param>
+        protected override void SolveInstance(IGH_DataAccess DA)
+        {
+            // Input
+            Mesh3D mesh = new Mesh3D();
+            DA.GetData(0, ref mesh);
+
+            // Output
+            DA.SetDataList(0, mesh.Elements);
+            DA.SetDataList(1, mesh.Nodes);
+            DA.SetData(2, mesh.Geometry);
+            DA.SetData(3, mesh.mesh);
+        }
+
+        /// <summary>
+        /// Provides an Icon for the component.
+        /// </summary>
+        protected override System.Drawing.Bitmap Icon
+        {
+            get
+            {
+                //You can add image files to your project resources and access them like this:
+                // return Resources.IconForThisComponent;
+                return Properties.Resources.Icon_DeconstructSolidMesh;
+            }
+        }
+
+        /// <summary>
+        /// Gets the unique ID for this component. Do not change this ID after release.
+        /// </summary>
+        public override Guid ComponentGuid
+        {
+            get { return new Guid("97c30c27-48c9-41ac-b09d-d02f80e806f6"); }
+        }
+    }
+}
