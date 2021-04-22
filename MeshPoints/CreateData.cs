b4d2450fc4a7b09df6c0ffb2bd2ca3129c125d06
Silje Knutsvik Kalleberg <siljekka@ntnu.no>
2021-04-22 11:28:01 +0200

    Fix string in list-structure

--- MeshPoints/CreateData.cs
+++ MeshPoints/CreateData.cs
@@ -5,6 +5,7 @@ using System.Collections.Generic;
 using MeshPoints.Classes;
 using System.Text;
 using MathNet;
+using System.Linq;
 
 namespace MeshPoints
 {
@@ -27,7 +28,7 @@ namespace MeshPoints
         {
             pManager.AddGenericParameter("Quality", "q", "Average quality of mesh.", GH_ParamAccess.item);
             pManager.AddGenericParameter("SmartMesh", "m", "Surface mesh.", GH_ParamAccess.item);
-            pManager.AddGenericParameter("Coordinate structure", "cs", "1: feautre for each x and y node coordinate. 2: one feature for all nodes.", GH_ParamAccess.item);
+            pManager.AddIntegerParameter("Coordinate structure", "cs", "1: feautre for each x and y node coordinate. 2: one feature for all nodes.", GH_ParamAccess.item);
             pManager.AddGenericParameter("filePath", "fp", "File path to where data are saved", GH_ParamAccess.item);
             pManager.AddBooleanParameter("WriteData", "w", "True: data is written to file, False: data is not written to file.", GH_ParamAccess.item);
 
@@ -72,7 +73,8 @@ namespace MeshPoints
             if (writeData)
             {
                 // 1. Add quality measure to string.
-                stringBuilder.Append(String.Format("{0}", quality));
+                double qualityRound = Math.Round(quality, 3);
+                stringBuilder.Append(String.Format("{0}", qualityRound));
 
                 if (structureType == 1)
                 {
@@ -88,13 +90,28 @@ namespace MeshPoints
                 else if (structureType == 2)
                 {
                     // 2. One feature for all nodes
+                    stringBuilder.Append(",");
                     for (int i = 0; i < nodes.Count; i++)
                     {
                         if (nodes[i].BC_U & nodes[i].BC_V) { continue; }
                         double x = Math.Round(nodes[i].Coordinate.X, 1) * (double) 10;
                         double y = Math.Round(nodes[i].Coordinate.Y, 1) * (double) 10;
-                        string text = String.Format("{0}{1}{2}", x, y, 0); // temporary 0
+                        double z = Math.Round(nodes[i].Coordinate.Z, 1) * (double) 10;
+                        string.Format("{0:00}", x);
+                        string.Format("{0:00}", y);
+                        string.Format("{0:00}", z);
+                        string xString = x.ToString("00");
+                        string yString = y.ToString("00");
+                        string zString = z.ToString("00");
+
+
+                        string text = String.Format("{0}{1}{2}", xString, yString, zString);
                         stringBuilder.Append(text);
+
+                        if (x > 99 | y > 99 | z > 99)
+                        {
+                           AddRuntimeMessage(GH_RuntimeMessageLevel.Error, "Check scaling. Coordinate value larger than 9.9.");
+                        }
                     }
 
 
