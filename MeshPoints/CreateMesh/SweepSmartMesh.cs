5d4ca4f66ba668b3f54e77d8880209dc24f092d1
hnedl <hnedl@LAPTOP-TMJQPLI9>
2021-05-07 13:32:44 +0200

    Add icon

--- MeshPoints/CreateMesh/SweepSmartMesh.cs
+++ MeshPoints/CreateMesh/SweepSmartMesh.cs
@@ -342,7 +342,7 @@ namespace MeshPoints.CreateMesh
             {
                 //You can add image files to your project resources and access them like this:
                 // return Resources.IconForThisComponent;
-                return null;
+                return Properties.Resources.Icon_SweepSolid;
             }
         }
 
