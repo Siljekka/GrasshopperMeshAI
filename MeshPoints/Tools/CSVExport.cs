818ba01c1f075f05b0f42202a2e37c7ab362aead
hnedl <hnedl@LAPTOP-TMJQPLI9>
2021-06-15 09:52:35 +0200

    Clean CSV Export

--- MeshPoints/Tools/CSVExport.cs
+++ MeshPoints/Tools/CSVExport.cs
@@ -26,14 +26,13 @@ namespace MeshPoints.Tools
         protected override void RegisterInputParams(GH_Component.GH_InputParamManager pManager)
         {
             pManager.AddGenericParameter("SmartMesh", "SM", "SmartMesh Class.", GH_ParamAccess.item);
-            pManager.AddIntegerParameter("Structure", "cs", "1: feautre for each x and y node coordinate. 2: one feature for all nodes. 3: Input to NN Mesh.", GH_ParamAccess.item);
+            pManager.AddIntegerParameter("Structure", "cs", "1: Node coordinates (x,y) for internal nodes only + avg quality for AR and JR." +
+                "2: All node coordinates (x,y) + avg quality for AR and JR." +
+                "3: All node coordinates (x,y) except corner nodes + avg quality for AR and JR.", GH_ParamAccess.item);
             pManager.AddGenericParameter("FilePath", "fp", "File path to where data are saved.", GH_ParamAccess.item);
             pManager.AddBooleanParameter("Save", "w", "True: data is written to file, False: data is not written to file.", GH_ParamAccess.item);
-            pManager.AddGenericParameter("AvgQuality", "avgQ", "Average quality of mesh.", GH_ParamAccess.item);
-            pManager.AddGenericParameter("MinQuality", "minQ", "Minimum quality of mesh.", GH_ParamAccess.item);
-            pManager.AddGenericParameter("TargetLength", "target", "Target length of mesh.", GH_ParamAccess.item);
-            pManager[5].Optional = true;
-            pManager[6].Optional = true;
+            pManager.AddGenericParameter("AvgAR", "avgQ", "Average Aspect Ratio of mesh.", GH_ParamAccess.item);
+            pManager.AddGenericParameter("AvgJR", "minQ", "Average Jacobian Ratio of mesh.", GH_ParamAccess.item);;
         }
 
         /// <summary>
@@ -53,24 +52,22 @@ namespace MeshPoints.Tools
             int structureType = 0;
             string filePath = "empty";
             bool writeData = false;
-            double avgQuality = 100;
-            double minQuality = 100;
-            double target = 0;
+            double avgAR = 100;
+            double avgJR = 100;
 
             DA.GetData(0, ref mesh);
             DA.GetData(1, ref structureType);
             DA.GetData(2, ref filePath);
             DA.GetData(3, ref writeData);
-            DA.GetData(4, ref avgQuality);
-            DA.GetData(5, ref minQuality);
-            DA.GetData(6, ref target);
+            DA.GetData(4, ref avgAR);
+            DA.GetData(5, ref avgJR);
 
             // 0. Check input
             if (!DA.GetData(0, ref mesh)) return;
             if (!DA.GetData(1, ref structureType)) return;
             if (!DA.GetData(2, ref filePath)) return;
-            if (!DA.GetData(4, ref avgQuality)) return;
-            if (structureType == 3 & (!DA.GetData(5, ref minQuality) | !DA.GetData(6, ref target))) { AddRuntimeMessage(GH_RuntimeMessageLevel.Warning, "Must have input minQuality and target when structureType is 3."); return; }
+            if (!DA.GetData(4, ref avgAR)) return;
+            if (!DA.GetData(5, ref avgJR)) return;
 
             StringBuilder stringBuilder = new StringBuilder();
             StringBuilder header = new StringBuilder();
@@ -83,19 +80,17 @@ namespace MeshPoints.Tools
                     // 0. Add header
                     if (new FileInfo(@filePath).Length == 0)
                     {
-                        header = AddHeader(mesh, filePath, 1);
+                        header = AddHeader(mesh, filePath, 2);
                     }
 
                     // 1. Add quality measure to string.
-                    double qualityRound = Math.Round(avgQuality, 3);
-                    double qualityRound1 = Math.Round(minQuality, 3);
-                    stringBuilder.Append(String.Format("{0}", qualityRound)); // AR
-                    stringBuilder.Append(String.Format(",{0}", qualityRound1)); // JR
+                    stringBuilder.Append(String.Format("{0}", avgAR)); // AR
+                    stringBuilder.Append(String.Format(",{0}", avgJR)); // JR
 
                     // 2. Feature for each x and y node coordinate
                     for (int i = 0; i < nodes.Count; i++)
                     {
-                        //if (nodes[i].BC_U & nodes[i].BC_V) { continue; }
+                        if (nodes[i].BC_U | nodes[i].BC_V) { continue; }
                         double x = Math.Round(nodes[i].Coordinate.X, 2);
                         double y = Math.Round(nodes[i].Coordinate.Y, 2);
                         string text = String.Format(",{0},{1}", x, y); // temporary 0
@@ -107,17 +102,17 @@ namespace MeshPoints.Tools
                     // 0. Add header
                     if (new FileInfo(@filePath).Length == 0)
                     {
-                        header = AddHeader(mesh, filePath, 2);
+                        header = AddHeader(mesh, filePath, 1);
                     }
 
                     // 1. Add quality measure to string.
-                    stringBuilder.Append(String.Format("{0}", avgQuality)); // AR
-                    stringBuilder.Append(String.Format(",{0}", minQuality)); // JR
+                    stringBuilder.Append(String.Format("{0}", avgAR)); // AR
+                    stringBuilder.Append(String.Format(",{0}", avgJR)); // JR
 
                     // 2. Feature for each x and y node coordinate
                     for (int i = 0; i < nodes.Count; i++)
                     {
-                        if (nodes[i].BC_U | nodes[i].BC_V) { continue; }
+                        //if (nodes[i].BC_U & nodes[i].BC_V) { continue; }
                         double x = Math.Round(nodes[i].Coordinate.X, 2);
                         double y = Math.Round(nodes[i].Coordinate.Y, 2);
                         string text = String.Format(",{0},{1}", x, y); // temporary 0
@@ -129,94 +124,20 @@ namespace MeshPoints.Tools
                     // 0. Add header
                     if (new FileInfo(@filePath).Length == 0)
                     {
-                        header = AddHeader(mesh, filePath, 3);
+                        header = AddHeader(mesh, filePath, 1);
                     }
 
-                    // 1. Add corner nodes of surface to string.
-                    List<Point3d> points = new List<Point3d>();
-                    foreach (Node node in mesh.Nodes)
-                    {
-                        if (node.BC_U & node.BC_V)
-                        {
-                            points.Add(node.Coordinate);
-                        }
-                    }
-                    stringBuilder.Append(String.Format("{0},{1}", points[0].X, points[0].Y));
-                    stringBuilder.Append(String.Format(",{0},{1}", points[1].X, points[1].Y));
-                    stringBuilder.Append(String.Format(",{0},{1}", points[3].X, points[3].Y));
-                    stringBuilder.Append(String.Format(",{0},{1}", points[2].X, points[2].Y));
+                    // 1. Add quality measure to string.
+                    stringBuilder.Append(String.Format("{0}", avgAR)); // AR
+                    stringBuilder.Append(String.Format(",{0}", avgJR)); // JR
 
-                    // 2. Add average quality
-                    stringBuilder.Append(String.Format(",{0}", avgQuality));
-
-                    // 3. Add minimum quality
-                    stringBuilder.Append(String.Format(",{0}", Math.Round(minQuality, 3)));
-
-                    // 4. Add target edge length
-                    stringBuilder.Append(String.Format(",{0}", target));
-
-                    // 5. Add number of nodes
-                    stringBuilder.Append(String.Format(",{0}", mesh.Nodes.Count));
-
-                    // 2. Add each x- and y- coordinate of nodes
+                    // 2. Feature for each x and y node coordinate
                     for (int i = 0; i < nodes.Count; i++)
                     {
                         if (nodes[i].BC_U & nodes[i].BC_V) { continue; }
                         double x = Math.Round(nodes[i].Coordinate.X, 2);
                         double y = Math.Round(nodes[i].Coordinate.Y, 2);
-                        string text = String.Format(",{0},{1}", x, y); // todo: fix Z?
-                        stringBuilder.Append(text);
-                    }
-                }
-                else if (structureType == 4)
-                {
-                    // 0. Add header
-                    if (new FileInfo(@filePath).Length == 0)
-                    {
-                        header = AddHeader(mesh, filePath, 4);
-                    }
-
-                    // 1. Add corner nodes of surface to string.
-                    List<Point3d> points = new List<Point3d>();
-                    foreach (Node node in mesh.Nodes)
-                    {
-                        if (node.BC_U & node.BC_V)
-                        {
-                            points.Add(node.Coordinate);
-                        }
-                    }
-                    stringBuilder.Append(String.Format("{0},{1}", points[0].X, points[0].Y));
-                    stringBuilder.Append(String.Format(",{0},{1}", points[1].X, points[1].Y));
-                    stringBuilder.Append(String.Format(",{0},{1}", points[3].X, points[3].Y));
-                    stringBuilder.Append(String.Format(",{0},{1}", points[2].X, points[2].Y));
-
-                    // 2. Add average quality
-                    stringBuilder.Append(String.Format(",{0}", avgQuality));
-
-                    // 3. Add minimum quality
-                    stringBuilder.Append(String.Format(",{0}", Math.Round(minQuality, 3)));
-
-                    // 4. Add number of nodes
-                    stringBuilder.Append(String.Format(",{0}", mesh.Nodes.Count));
-
-                    // 5. Add number of inner nodes
-                    int counter = 0;
-                    foreach (Node node in mesh.Nodes)
-                    {
-                        if (!node.BC_U | !node.BC_V)
-                        {
-                            counter++;
-                        }
-                    }
-                    stringBuilder.Append(String.Format(",{0}", counter));
-
-                    // 6. Add each x- and y- coordinate of nodes
-                    for (int i = 0; i < nodes.Count; i++)
-                    {
-                        if (nodes[i].BC_U | nodes[i].BC_V) { continue; }
-                        double x = Math.Round(nodes[i].Coordinate.X, 2);
-                        double y = Math.Round(nodes[i].Coordinate.Y, 2);
-                        string text = String.Format(",{0},{1}", x, y); // todo: fix Z?
+                        string text = String.Format(",{0},{1}", x, y); // temporary 0
                         stringBuilder.Append(text);
                     }
                 }
@@ -243,70 +164,30 @@ namespace MeshPoints.Tools
                 int nodes = 1;
                 for (int i = 0; i < mesh.Nodes.Count; i++)
                 {
-                    //if (mesh.Nodes[i].BC_U & mesh.Nodes[i].BC_V) { continue; }
+                    if (mesh.Nodes[i].BC_U | mesh.Nodes[i].BC_V) { continue; }
                     header.Append(String.Format(",x{0},y{0}", nodes));
                     nodes++;
                 }
             }
-            if (structureType == 2)
+            else if (structureType == 2)
             {
                 header.Append("avgAR,avgJR");
                 int nodes = 1;
                 for (int i = 0; i < mesh.Nodes.Count; i++)
                 {
-                    if (mesh.Nodes[i].BC_U | mesh.Nodes[i].BC_V) { continue; }
                     header.Append(String.Format(",x{0},y{0}", nodes));
                     nodes++;
                 }
             }
             else if (structureType == 3)
             {
-                int corners = 1;
-                foreach (Node node in mesh.Nodes)
-                {
-                    if (node.BC_U & node.BC_V)
-                    {
-                        if (new FileInfo(@filePath).Length == 0) { header.Append(String.Format("x{0},y{0},", corners)); }
-                        corners++;
-                    }
-                }
-
-                header.Append("avgQuality,minQuality");
-                header.Append(",targetEdgeLength");
-                header.Append(",totalNodes");
+                header.Append("avgAR,avgJR");
                 int nodes = 1;
                 for (int i = 0; i < mesh.Nodes.Count; i++)
                 {
                     if (mesh.Nodes[i].BC_U & mesh.Nodes[i].BC_V) { continue; }
                     header.Append(String.Format(",x{0},y{0}", nodes));
                     nodes++;
-                }
-            }
-            else if (structureType == 4)
-            {
-                // 1. For corners
-                int corners = 1;
-                foreach (Node node in mesh.Nodes)
-                {
-                    if (node.BC_U & node.BC_V)
-                    {
-                        if (new FileInfo(@filePath).Length == 0) { header.Append(String.Format("x{0},y{0},", corners)); }
-                        corners++;
-                    }
-                }
-
-                // 2. For quality and # nodes
-                header.Append("avgQuality,minQuality");
-                header.Append(",totalNodes");
-                header.Append(",innerNodes");
-
-                // 3. For inner nodes
-                int innerNodes = 1;
-                for (int i = 0; i < mesh.Nodes.Count; i++)
-                {
-                    if (mesh.Nodes[i].BC_U | mesh.Nodes[i].BC_V) { continue; }
-                    header.Append(String.Format(",x{0},y{0}", innerNodes));
-                    innerNodes++;
                 }
             }
             return header;
